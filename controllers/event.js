3de9c88c00ef08b0bb14bd41a5d4fb80582fc6e0
yizhou zhou <yizhouzhou@yizhous-mbp.myfiosgateway.com>
2021-12-17 10:08:09 -0500

    remove swagger comment

--- controllers/event.js
+++ controllers/event.js
@@ -3,58 +3,6 @@ const Event = require("../models/event");
 exports.getEvents = (req, res) => {
   const limit = +req.query.limit;
   const offset = +req.query.offset;
-
-  /*  #swagger.parameters['offset'] = {
-                in: 'query',
-                type: "string",
-                name:"offset",
-                required:false,
-                description: 'pagination offset'
-    } */
-  /*  #swagger.parameters['limit'] = {
-                in: 'query',
-                type: "string",
-                name:"limit",
-                required:false,
-                description: 'pagination limit'
-      } */
-
-  /* #swagger.responses[200] = {
-    description:"Event fetched",
-    schema:{
-        message: "Event fetched!",
-        result: [
-            {
-                _id: "619c7c4079c22be0ffb92e24",
-                from: "2021-11-23T04:12:12.923Z",
-                to: "2021-11-23T04:12:30.327Z",
-                content: "go to bed!!!!!",
-                isCompleted: false,
-                creator: "619c68e8101cae55f5efc1a3",
-                __v: "0"
-            },
-            {
-                _id: "619c7c4879c22be0ffb92e26",
-                from: "2021-11-23T04:12:12.923Z",
-                to: "2021-11-23T04:12:30.327Z",
-                content: "go to bed!!!!!??",
-                isCompleted: false,
-                creator: "619c68e8101cae55f5efc1a3",
-                __v: "0"
-            }
-        ],
-        maxCount: "2"
-      }
-  } */
-  /* 
-    #swagger.responses[500] = {
-      schema:{
-      message: "You are not authenticated: JsonWebTokenError: invalid token"
-      },
-      description:"failed to fetch event"
-    }
-  */
-
   const query = Event.find({ creator: req.userData.userId });
   let fetchedResult;
   if (limit && offset) {
@@ -81,40 +29,6 @@ exports.getEvents = (req, res) => {
 };
 
 exports.postEvent = (req, res) => {
-  /*  #swagger.parameters['event'] = {
-                in: 'body',
-                schema:{
-                  from: "2021-11-23T19:25:19.178Z",
-                  to: "2021-11-23T19:25:19.178Z",
-                  content: "eat breakfast"
-                },
-                description: 'New event'
-              } */
-  /* 
-      #swagger.responses[201] = {
-              description:"event created",
-              schema:{
-              message: "event created successfully!",
-              data: {
-              from: "2021-11-23T04:12:12.923Z",
-              to: "2021-11-23T04:12:30.327Z",
-              content: "go to bed!!!!!",
-              isCompleted: false,
-              creator: "619c68e8101cae55f5efc1a3",
-              __v: 0,
-              id: "619d634172c81b93c4838353"
-              }
-            }
-        }
-  */
-  /* 
-      #swagger.responses[500] = {
-              description:"failed to create event",
-              schema:{
-                message: "You are not authenticated: JsonWebTokenError: invalid token"
-              }
-        }
-  */
   const event = new Event({
     ...req.body,
     creator: req.userData.userId,
@@ -125,6 +39,7 @@ exports.postEvent = (req, res) => {
   if(new Date(req.body.from).getTime()>new Date(req.body.to).getTime()){
     return res.status(400).json({message:"'from' time should be before 'to' time"});
   };
+
   event
     .save()
     .then((createdEvent) => {
@@ -145,38 +60,6 @@ exports.postEvent = (req, res) => {
 };
 
 exports.updateEvent = (req, res) => {
-  /*  #swagger.parameters['event'] = {
-              in: 'body',
-              schema:{
-                from: "2021-11-23T19:25:19.178Z",
-                to: "2021-11-23T19:25:19.178Z",
-                content: "eat lunch",
-                creator:"619c7c4079c22be0ffb92e24"
-              },
-              description: 'New event'
-            } */
-  /*  #swagger.parameters['id'] = {
-        in: 'path',
-        type: "string",
-        description: 'target event id'
-      } */
-  /* 
-      #swagger.responses[500] = {
-        description:"Couldn't update post",
-        schema:{
-    message: "Couldn't update post: MongoServerError: Performing an update on the path '_id' would modify the immutable field '_id'"
-}
-      }
-  */
-  /* 
-      #swagger.responses[200] = {
-        description:"update successfully",
-        schema:{
-          message: "update successfully!"
-        }
-      }
-  */
-
   let { _id, ...rest } = req.body;
   _id = req.params.id;
   Event.updateOne(
@@ -201,36 +84,6 @@ exports.updateEvent = (req, res) => {
 };
 
 exports.deleteEvent = (req, res) => {
-  /*  #swagger.parameters['id'] = {
-                in: 'path',
-                type: "string",
-                description: 'target event id'
-        } */
-  /* 
-      #swagger.responses[200] = {
-        description:"delete successfully",
-        schema:{
-          message: "delete successfully!"
-        }
-      }
-  */
-  /* 
-      #swagger.responses[401] = {
-        description:"failed to delete",
-        schema:{
-          message: "Failed to delete!"
-        }
-      }
-  */
-  /* 
-      #swagger.responses[500] = {
-        description:"failed to delete",
-        schema:{
-          message: "You are not authenticated: JsonWebTokenError: invalid token"
-        }
-      }
-  */
-
   Event.deleteOne({ _id: req.params.id, creator: req.userData.userId })
     .then((result) => {
       if (result.deletedCount > 0) {
--- controllers/user.js
+++ controllers/user.js
@@ -6,38 +6,6 @@ const JWT_KEY = "z123fg";
 exports.JWT_KEY = JWT_KEY;
 
 exports.createUser = (req, res) => {
-  /*  #swagger.parameters['New user'] = {
-                  in: 'body',
-                  schema:{
-                      username:"funnycat",
-                      password:"123123",
-                      isAdmin:true
-                  },
-                  description: 'New user info'
-    } */
-
-  /* #swagger.responses[201] = {
-        description: 'user created',
-        schema:  {
-        message: "User created!",
-        data: {
-            username: "funnycat",
-            role: [
-                "user"
-            ],
-            _id: "619d5c0882043dfe3d855f0a",
-            __v: 0
-      }
-            
-        }
-    } */
-  /* #swagger.responses[500] = {
-      description: 'username is not unique',
-      schema:  {
-    message: "Invalid authentication credentials: ValidationError: username: Error, expected `username` to be unique. Value: `z123fg`"
-        }
-    } */
-
   const { isAdmin } = req.body;
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
@@ -63,44 +31,6 @@ exports.createUser = (req, res) => {
 };
 
 exports.userLogin = (req, res, next) => {
-  /*  #swagger.parameters['New user'] = {
-                  in: 'body',
-                  schema:{
-                      username:"funnycat",
-                      password:"123123"
-                  },
-                  description: 'User Login info'
-          } */
-  /* #swagger.responses[200] = {
-        description: 'login successfully',
-        schema: {
-            message:"login successfully!",
-            data:{
-                lastLogin: "2021-11-23T05:27:00.405Z",
-                username: "z123fg",
-            role: [
-              "user",
-              "admin"
-          ],
-          token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InoxMjNmZyIsInVzZXJJZCI6IjYxOWM2OGU4MTAxY2FlNTVmNWVmYzFhMyIsInJvbGUiOlsidXNlciIsImFkbWluIl0sImlhdCI6MTYzNzY0NTIyMCwiZXhwIjoxNjM3NjQ4ODIwfQ.83WUPtx3H7REAQUTLem_WqVhQfhs0k3C3Gi1FPtkaYo",
-          expiresIn: 3600,
-          userId: "619c68e8101cae55f5efc1a3"
-            }
-            
-        }
-      } */
-  /* #swagger.responses[401] = {
-      description: 'Invalid password',
-      schema:  {
-        message: "Invalid password!"
-        }
-    } */
-  /* #swagger.responses[403] = {
-      description: "Username doesn't exist",
-      schema:  {
-        message: "Username doesn't exist!"
-        }
-    } */
   let fetchedUser;
   User.findOne({ username: req.body.username })
     .then((user) => {
